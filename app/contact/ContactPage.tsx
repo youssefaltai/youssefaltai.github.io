6d835e765dde63efbb5d34afc1a57077f8aaafee
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-09-27 14:13:33 +0300

    Fix metadata in client component issue

---
+++ app/contact/ContactPage.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Loader from "../Loader";
+import { ContactFormProvider } from "./FormContext";
+import Logo from "@/components/Logo";
+import ContactForm from "./ContactForm";
+
+export default function ContactPage() {
+  return (
+    <>
+      <Loader scrollToTop />
+      <ContactFormProvider>
+        <main className="flex flex-col justify-center items-center gap-16 py-16 w-full max-w-screen-sm">
+          <Logo />
+          <ContactForm />
+        </main>
+      </ContactFormProvider>
+    </>
+  );
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,26 +1,9 @@
-"use client";
-
-import Loader from "../Loader";
-import { ContactFormProvider } from "./FormContext";
-import Logo from "@/components/Logo";
-import ContactForm from "./ContactForm";
 import { Metadata } from "next";
+import ContactPage from "./ContactPage";
 
 export const metadata: Metadata = {
   title: "Contact Me",
   description: "Get in touch with me. Let's work together!",
 };
 
-export default function Contact() {
-  return (
-    <>
-      <Loader scrollToTop />
-      <ContactFormProvider>
-        <main className="flex flex-col justify-center items-center gap-16 py-16 w-full max-w-screen-sm">
-          <Logo />
-          <ContactForm />
-        </main>
-      </ContactFormProvider>
-    </>
-  );
-}
+export default () => <ContactPage />;
