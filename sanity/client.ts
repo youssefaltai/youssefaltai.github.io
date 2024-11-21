95172aa04e693c0f9522cd9873503a1dd60b7de9
Youssef al-Tai <youssefgalalaltai@gmail.com>
2024-11-21 05:14:14 +0200

    Revert "Add Sanity client integration and update homepage to fetch posts"
    This reverts commit 966007f39c32078cff1da529aa11e5823d0fe3b6.

--- sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: "akm84dsw",
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: false,
-});
