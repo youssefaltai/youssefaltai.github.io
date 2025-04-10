34a762a746f094e88ba4ef2d3c8fd828a722d41a
Youssef <youssefgalalaltai@gmail.com>
2025-04-10 23:57:27 +0200

    Update "About" section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import BackToHomeLink from "../BackToHomeLink";
 import Bio from "../Bio";
 import CallToAction from "@/components/CallToAction";
 import Loader from "../Loader";
@@ -22,13 +21,11 @@ function About() {
 
         <div className="flex flex-col justify-center gap-4 px-6  max-w-lg">
           <p className="text-lg md:text-xl">
-            Frontend engineer with 2+ years of experience in designing and
-            developing scalable, user-centric frontends for the web using
-            Next.js, and for cross-platform mobile apps using Flutter.
+            Frontend engineer with 2+ years of experience building fast, scalable, and user-focused apps.
+            Passionate about clean UI, intuitive UX, and engineering solutions users actually want.
           </p>
           <p className="text-lg md:text-xl">
-            Recognized for my knack for thorough research, tend to mentor fellow
-            colleagues, and writing clean, maintainable code.
+            Strong in React, Flutter, and cloud-native dev. Comfortable in fast-paced, product-driven teams — from startup MVPs to large monorepos.
           </p>
         </div>
         <CallToAction href="/contact">Contact Me</CallToAction>
